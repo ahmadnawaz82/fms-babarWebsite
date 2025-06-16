8c98820d363860c4f07d137c26d298e34909185c
Babar-thedeveloper <babarkhursheed345@gmail.com>
2025-06-16 16:35:38 +0500

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,10 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: './', // ✅ Ensures relative paths so index.html, JS, CSS load properly on cPanel
+  build: {
+    outDir: 'dist', // ✅ Ensures output goes into dist/ folder (used in .cpanel.yml)
+  },
   plugins: [react(), tailwindcss()],
 })
